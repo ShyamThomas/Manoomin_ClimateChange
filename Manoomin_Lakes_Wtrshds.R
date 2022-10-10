849477f47cbdce9dd7a7d6bb07b9571ee2ce6b21
Shyam Thomas <thomas.shyam@gmail.com>
2022-10-10 10:13:23 -0500

    Changes

--- Manoomin_Lakes_Wtrshds.R
+++ Manoomin_Lakes_Wtrshds.R
@@ -1,9 +1,9 @@
 library(sf)
-library(ggplot2)
-library(tidyverse)
+library(ggplot2) 
+library(tidyverse) 
+library(raster)
 
 setwd("~/UMNpostdoc/ProjectEWM/RProjects/Manoomin_ClimateChange")
-
 
 lakes=st_read("GIS_Data/MN_dnr_watersheds/dnr_watersheds_auto_catchment_lakes.shp")
 
@@ -23,7 +23,6 @@ PerchLake_ctrd=st_centroid(PerchLake)
 plot(st_geometry(wtrshds), axes=TRUE, graticule=TRUE)
 plot(st_geometry(PerchLake_ctrd), pch = 16, add = TRUE, col="red")
 
-library(raster)
 Minn.LAI = stack("GIS_Data/GLASS01D01.V60.A2000057.h11v04.2022012.LAI.jpg")  
 
 ### Steps to find the correct MODIS tile for the state of Minnesota & Wisconsin
@@ -130,14 +129,18 @@ Minn_mask_UTM=projectRaster(Minn_mask, crs="+proj=utm +zone=15 +datum=WGS84 +uni
 Minn_mask_UTM
 plot(Minn_mask_UTM)
 
+##################################################################################################################################################
 #### Get the Wisconsin state shape file and clip the relevant WI state raster area
 Wisc.shp=st_read("GIS_Data/Wisconsin_State_Boundary_24K.shp")
+LAI_newR=projectRaster(LAI_r, crs="+proj=longlat +datum=WGS84 +no_defs")
+
 Wisc_WGS84=st_transform(Wisc.shp, crs(LAI_newR))
 st_crs(Wisc_WGS84)
-plot(Wisc_WGS84$geometry, add=T)
+plot(Wisc_WGS84$geometry)
 
 Wisc_crop=crop(LAI_WGS84,Wisc_WGS84)
 Wisc_mask=mask(Wisc_crop, Wisc_WGS84)
+
 plot(Wisc_mask)
 
 Wisc_mask_UTM=projectRaster(Wisc_mask, crs="+proj=utm +zone=16 +datum=WGS84 +units=m +no_defs") 
@@ -156,6 +159,92 @@ st_crs(WI.wtrshds_bear)
 
 mycol = rgb(1,0,0,0.09)
 plot(Wisc_mask_UTM)
+WI.wtrshds_bear_UTM=st_transform(WI.wtrshds_bear, crs=st_crs(Wisc_mask_UTM))
+WI.wtrshds_bear_UTM
 plot(WI.wtrshds_bear_UTM, col = mycol, add=TRUE)
-LAI.vals=raster::extract(Wisc_mask_UTM,WI.wtrshds_bear_UTM)
-hist(LAI.vals[[1]])
+
+bbox.Bear.wtrshd=st_as_sfc(st_bbox(WI.wtrshds_bear_UTM$geometry))
+plot(bbox.Bear.wtrshd)
+plot(bbox.Bear.wtrshd, axes=TRUE)
+plot(WI.wtrshds_bear_UTM$geometry, add=TRUE, col=mycol)
+
+(285931.4-260830.9)/1000
+(5113292-5086001)/1000
+st_centroid(bbox.Bear.wtrshd)
+st_transform(st_centroid(bbox.Bear.wtrshd),crs = "+proj=longlat +datum=WGS84")
+
+files_sin = list.files("GIS_Data/GTiff_sin", pattern="_Lai_500m.tif", full.names=TRUE)
+files_sin
+rs_lai.sin=stack(files)
+
+
+
+Bear.LAI=read_csv("RawData/Bear_Lai_500m.csv") ### clean data dowloaded from ORNL using bounding box polygon
+LAI.80per=Bear.LAI%>%filter(per_cent_pixels_pass_qa>0.8)
+LAI.80per
+
+
+TimeSeries <- ggplot(LAI.80per, aes(x=as.Date(date, "%m/%d/%y"), y = mean)) +
+geom_line(colour = "#2D708EFF", aes(y=mean)) + # draw the line for the mean value
+ggtitle(" ") + # add a title
+ylab("Mean LAI") + # add a y-axis label
+xlab("Date") + # add a x-axis label
+theme_bw() + # set the plot theme
+theme(plot.title = element_text(lineheight=.8, face="bold", size=20), axis.title.x = element_text(face="bold", size=20), axis.title.y = element_text(face="bold", size=20), axis.text.x = element_text(size=16), axis.text.y = element_text(size=16))
+TimeSeries
+ggsave(paste("Figures/R_MODIS_Plots_", product,"_Fig1_timeseries.png", sep=''), width=25, height=16, units='cm')
+
+
+Boxplot_Monthly <- ggplot(LAI.80per, aes(factor(month), mean)) +
+geom_boxplot(fill="#2D708EFF") +
+geom_jitter(width = 0) +
+ggtitle("Mean Values by Month") +
+xlab("Month") +
+ylab("Mean LAI") +
+theme_bw() +
+theme(plot.title = element_text(lineheight=.8, face="bold", size=20), legend.position="none", axis.title.x = element_text(face="bold", size=20),    axis.text.x = element_text(angle=90, vjust=0.5, size=16), axis.title.y = element_text(face="bold", size=20), axis.text.y = element_text(size=16)) +
+guides(fill=FALSE)
+
+Boxplot_Monthly
+ggsave(paste("Figures/R_MODIS_Plots_", product,"_Fig2_monthyMean.png", sep=''), width=25, height=16, units='cm')
+
+
+StackedByYear <- ggplot(LAI.80per, aes(x=as.numeric(substr(LAI.80per$`date[YYYYDDD]`,6,8)), y=mean, color=as.factor(year))) +
+scale_color_viridis(discrete=T) +
+geom_point(size=2) +
+geom_smooth(aes(factor=year), se=FALSE, size=1.2) +
+ggtitle("LAI Mean Value by Day of Year") +
+theme(plot.title = element_text(lineheight=.8, face="bold", size=20)) +
+xlab("Day of Year") +
+ylab("Mean LAI") +
+theme_bw() +
+theme(plot.title = element_text(lineheight=.8, face="bold", size=20), axis.title.x = element_text(face="bold", size=20), axis.text.x = element_text(angle=90, vjust=0.5, size=16), axis.title.y = element_text(face="bold", size=20), axis.text.y = element_text(size=16)) +theme(legend.title = element_blank())
+StackedByYear
+ggsave(paste("Figures/R_MODIS_Plots_", product,"_Fig5_stackedYears.png", sep=''), width=25, height=16, units='cm')
+
+
+
+mar=ggplot(Max.LAI_mar, aes(x=year, y=maxLAI))+geom_point()+geom_smooth(method='lm')+ylab("max avg.LAI")+ggtitle("March")
+apr=ggplot(Max.LAI_apr, aes(x=year, y=maxLAI))+geom_point()+geom_smooth(method='lm')+ylab("max avg.LAI")+ggtitle("April")
+may=ggplot(Max.LAI_may, aes(x=year, y=maxLAI))+geom_point()+geom_smooth(method='lm')+ylab("max avg.LAI")+ggtitle("May")
+jun=ggplot(Max.LAI_jun, aes(x=year, y=maxLAI))+geom_point()+geom_smooth(method='lm')+ylab("max avg.LAI")+ggtitle("June")
+
+grid.arrange(mar, apr, may, jun)
+
+
+####################################################################################################################################
+### Plot a map showing all the Rice field sites
+US_shp=st_read("GIS_Data/gadm41_USA_shp/gadm41_USA_1.shp")
+US_shp
+MN_WI_shp=US_shp%>%filter(NAME_1=="Minnesota" | NAME_1=="Wisconsin")
+MN_WI_shp
+plot(MN_WI_shp$geometry)
+
+RiceFieldSites=read_csv("GIS_Data/RiceFieldSites.csv")
+RiceFieldSites_sf=st_as_sf(RiceFieldSites, coords = c("Lon","Lat"))
+RiceFieldSites_sf
+st_set_crs(RiceFieldSites_sf, 4326)
+plot(MN_WI_shp$geometry, axes=TRUE)
+plot(RiceFieldSites_sf$geometry, add=TRUE, col="red")
+
+
